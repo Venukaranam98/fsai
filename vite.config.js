4cd5a0f9196a30ef46f9b742adf8467c40773afc
venukaranam98 <vk3344556677@gmail.com>
2026-03-22 17:45:48 +0530

    chore: add backend url

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     port: 3000,
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://fsai-ft9d.onrender.com',
         changeOrigin: true,
       },
     },
